6c96b139828310f87f37d8f08efa65a08253ed94
Raghav Pillai <raghavpillai101@gmail.com>
2024-10-12 14:09:18 -0700

    Enhance login page UI with new styles and branding
    - Increased grayscale and brightness of background
    - Added blur and transparency effects
    - Redesigned login logo and text
    - Improved overall visual appeal and branding

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Image } from "@chakra-ui/react";
+import { Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
 
 export default function LoginPage() {
   return (
@@ -13,20 +13,39 @@ export default function LoginPage() {
         w="100%"
         h="100%"
         objectFit="cover"
-        filter="grayscale(50%) brightness(50%)"
+        filter="grayscale(30%) brightness(100%)"
       />
       <Box
         h="full"
         w="50%"
-        bg="rgba(0, 0, 0, 0.2)"
+        bg="rgba(0, 0, 0, 0.4)"
         position="absolute"
         right="0"
         top="0"
         zIndex="1"
-        backdropFilter="blur(5px)"
+        backdropFilter="blur(10px) grayscale(50%)"
         borderLeft="1px"
         borderColor="rgba(255,255,255,0.2)"
-      ></Box>
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <VStack>
+          <Text fontSize="xl">Login to</Text>
+          <HStack spacing={1}>
+            <Image
+              src="/x-icon.png"
+              alt="logo"
+              w="20px"
+              h="20px"
+              filter="invert(100%)"
+            />
+            <Text fontSize="2xl" fontWeight="bold">
+              pect
+            </Text>
+          </HStack>
+        </VStack>
+      </Box>
     </Box>
   );
 }
