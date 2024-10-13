3fff84c9fb4c0bdf868181ace69ccc726b3b1895
Raghav Pillai <raghavpillai101@gmail.com>
2024-10-12 19:09:25 -0700

    Added background image and centered content
    - Added a full-screen background image with a gradient overlay
    - Centered the content within the page

--- client/app/dash/page.tsx
+++ client/app/dash/page.tsx
@@ -225,6 +225,32 @@ const Graphs = ({
   );
 };
 
+const BackgroundImage = () => {
+  return (
+    <Box
+      position="absolute"
+      top={0}
+      right={0}
+      w="100%"
+      height="100%"
+      zIndex={-1}
+      backgroundImage="url('/bg-3.png')"
+      backgroundSize="cover"
+      backgroundPosition="center top"
+      _after={{
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background:
+          "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,1))",
+      }}
+    />
+  );
+};
+
 export default function DashPage() {
   const [dataLoading, setDataLoading] = useState(true);
   const router = useRouter();
@@ -470,7 +496,19 @@ export default function DashPage() {
       display="flex"
       position="relative"
       spacing={0}
+      zIndex={20}
     >
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <BackgroundImage />
+      </Box>
       <Button
         onClick={() => router.push("/search")}
         position="absolute"
